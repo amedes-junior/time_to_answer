2c2e9980cab2a46ee816d8406f13984cf22e67e6
Amedes Junior <amedes@gmail.com>
2019-03-12 16:24:51 -0300

    176. 3 formas de criar um registro no ActiveRecord

--- app/controllers/admins_backoffice/questions_controller.rb
+++ app/controllers/admins_backoffice/questions_controller.rb
@@ -1,8 +1,9 @@
 class AdminsBackoffice::QuestionsController < AdminsBackofficeController
-  before_action: set_question, only: [: edit,: update,: destroy]
+  before_action :set_question, only: [:edit, :update, :destroy]
+  before_action :get_subjects, only: [:new, :edit]
+
   def index
-  @questions =
-    Question.all.order(: description).page(params[: page])
+    @questions = Question.includes(:subject).order(:description).page(params[:page])
   end
 
   def new
@@ -12,9 +13,9 @@ class AdminsBackoffice::QuestionsController < AdminsBackofficeController
   def create
     @question = Question.new(params_question)
     if @question.save
-      redirect_to admins_backoffice_question_path, notice: "Questão cadastrada com sucesso!"
+      redirect_to admins_backoffice_questions_path, notice: "Questão cadastrada com sucesso!"
     else
-      render: new
+      render :new
     end
   end
 
@@ -24,28 +25,32 @@ class AdminsBackoffice::QuestionsController < AdminsBackofficeController
 
   def update
     if @question.update(params_question)
-      redirect_to admins_backoffice_question_path, notice: "Questão atualizada com sucesso!"
+      redirect_to admins_backoffice_questions_path, notice: "Questão atualizada com sucesso!"
     else
-      render: edit
+      render :edit
     end
   end
 
   def destroy
     if @question.destroy
-      redirect_to admins_backoffice_question_path, notice: "Questão excluída com sucesso!"
+      redirect_to admins_backoffice_questions_path, notice: "Questão excluída com sucesso!"
     else
-      render: index
+      render :index
     end
   end
 
   private
 
+  def get_subjects
+    @subjects = Subject.all
+  end
+
   def params_question
-    params.require(: question).permit(: description,: subject_id)
+    params.require(:question).permit(:description,:subject_id)
   end
 
   def set_question
-    @question = Question.find(params[: id])
+    @question = Question.find(params[:id])
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     get 'welcome/index'
     resources :admins
     resources :subjects
+    resources :questions
   end
   devise_for :users
 
