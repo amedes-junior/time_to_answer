6d2b4d5f4fac05ecf94730fffd51e6eafa57454e
Amedes Junior <amedes@gmail.com>
2019-05-24 17:55:23 -0300

    203. Reflexão sobre o uso do sistema

--- app/controllers/site/search_controller.rb
+++ app/controllers/site/search_controller.rb
@@ -2,4 +2,9 @@ class Site::SearchController < SiteController
   def questions
     @questions = Question._search_(params[:term], params[:page])
   end
+
+  def subject
+    @questions = Question.search_subject(params[:subject_id], params[:page])
+  end
+
 end
